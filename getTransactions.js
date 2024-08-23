6e38ed34ff95c68800dff7d0a7e5e3cc91efb57e
mohammedSaleh1212 <mahtrbtktk@gmail.com>
2024-08-23 14:47:47 +0300

    new commit

--- getTransactions.js
+++ getTransactions.js
@@ -1,50 +1,58 @@
 
+const { ethers } = require("ethers");
 
 
-    const { ethers } = require('ethers');
+const INFURA_URL = process.env.INFURA_URL || "https://mainnet.infura.io/v3/423150519d6842cbb425088ebbc57329";
+const TRANSFER_COUNT = 10; 
 
-const infuraProjectId = '423150519d6842cbb425088ebbc57329';
-const provider = new ethers.providers.InfuraProvider('homestead', infuraProjectId);
 
-async function getLatestBlockNumber() {
-  return await provider.getBlockNumber();
+const provider = new ethers.providers.JsonRpcProvider(INFURA_URL);
+
+
+async function getLastTransfers(transferCount = TRANSFER_COUNT) {
+    try {
+        const latestBlock = await provider.getBlockNumber();
+        let transfers = [];
+        let promises = [];
+
+        for (let i = latestBlock; i >= 0 && transfers.length < transferCount; i--) {
+      
+            promises.push(provider.getBlockWithTransactions(i).then(block => {
+                for (let tx of block.transactions) {
+                    if (tx.value.gt(0)) {
+                        transfers.push({
+                            blockNumber: tx.blockNumber,
+                            from: tx.from,
+                            to: tx.to,
+                            value: ethers.utils.formatEther(tx.value),
+                            hash: tx.hash
+                        });
+                        if (transfers.length === transferCount) {
+                            return; 
+                        }
+                    }
+                }
+            }).catch(error => {
+                console.error(`Error fetching block ${i}:`, error);
+            }));
+
+            if (i % 10 === 0) {
+                await Promise.all(promises);
+                promises = [];
+            }
+        }
+
+        await Promise.all(promises);
+        return transfers.slice(0, transferCount);
+    } catch (error) {
+        console.error("Error in getLastTransfers:", error);
+        throw error;
+    }
 }
 
-async function getTransactionsFromBlock(blockNumber) {
-  const block = await provider.getBlockWithTransactions(blockNumber);
-  return block.transactions;
-}
-
-async function getLastTenTransferTransactions() {
-  const latestBlockNumber = await getLatestBlockNumber();
-  let transactions = [];
-  let blockNumber = latestBlockNumber;
-
-  while (transactions.length < 10 && blockNumber >= 0) {
-    const blockTransactions = await getTransactionsFromBlock(blockNumber);
-    const transferTransactions = blockTransactions.filter(tx => tx.data === '0x');
-    transactions = transactions.concat(transferTransactions);
-    blockNumber--;
-  }
-
-  return transactions.slice(0, 10);
-}
-
-getLastTenTransferTransactions().then(transactions => {
-  transactions.forEach(tx => {
-    console.log(`Hash: ${tx.hash}, From: ${tx.from}, To: ${tx.to}, Value: ${ethers.utils.formatEther(tx.value)} ETH,Block: ${parseInt(tx.blockNumber)}`);
-  });
+getLastTransfers().then(transfers => {
+    console.log(transfers);
 }).catch(error => {
-  console.error('Error fetching transactions:', error);
+    console.error("Error:", error);
 });
-
-
-
-
-
-
-
-
-
-
 
